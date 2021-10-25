d1419eb36b7a2eb3691622c0210a1a3decb80f51
shiquine <godchildshiquine@gmail.com>
2021-10-25 09:54:23 -0700

    test

---
+++ main.cs
@@ -0,0 +1,10 @@
+// Hello World!
+namespace Hello World
+{
+    class hello {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine('Hello world')
+        }
+    }
+}
